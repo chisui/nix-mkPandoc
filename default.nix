c519c754e97830caca0d192a4b4a04b4814b2702
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-08-26 20:25:26 +0200

    cleanup in default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,7 @@
-{ nixpkgs ? <nixpkgs> 
-, pkgs ? import nixpkgs {}
-}:
-
-let
-  eisvogel = import ./eisvogel.nix {};
-  mkPandoc = import ./mkPandoc.nix {};
-in
-  mkPandoc {
-    name     = "test.pdf";
-    version  = "0.1.0";
-    src      = ./test.md;
-    template = eisvogel;
-  }
-
+(import ./mkPandoc.nix {}) {
+  name     = "test.pdf";
+  version  = "0.1.0";
+  toc      = true;
+  src      = ./test.md;
+  template = import ./eisvogel.nix {};
+}
