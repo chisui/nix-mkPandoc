5491c140c52c3400a011869f193cc7e1510b5cb3
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-08-28 16:01:37 +0200

    fixed broken derivation

--- journal-of-computer-information-systems.csl.nix
+++ journal-of-computer-information-systems.csl.nix
@@ -1,13 +1,20 @@
 { nixpkgs ? <nixpkgs>
 , pkgs ? import nixpkgs {}
 , fetchurl ? pkgs.fetchurl
-, mkSingleFile ? import ./mkSingleFile.nix { inherit pkgs; }
+, mkDerivation ? pkgs.stdenv.mkDerivation 
 }:
-mkSingleFile {
+mkDerivation {
   name = "journal-of-computer-information-systems.csl";
   version = "0.1.0";
+
   src = fetchurl {
     url    = "https://raw.githubusercontent.com/citation-style-language/styles/73a405779d590a45424650c712b43e6417b412c0/journal-of-computer-information-systems.csl";
     sha256 = "803069db5fedba92f478ba8b91a3235cb0b892e1d8edc57b611b832d3f763b95";
   };        
+
+  unpackPhase    = ":";
+  buildPhase     = ":";
+  installPhase   = "cp $src $out";
+  configurePhase = ":";
+  fixupPhase     = ":";
 }
