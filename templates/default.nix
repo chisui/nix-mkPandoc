9500c2925aab1a87ab70b2c308b81b1e26a4ee9d
Philipp Dargel <chisui@fb3.de>
2018-09-03 00:50:15 +0200

    pass pkgs through

--- templates/default.nix
+++ templates/default.nix
@@ -3,7 +3,7 @@ with builtins;
 let
   importTemplate = t: 
     let
-      value = import t {};
+      value = import t { inherit pkgs; };
       name = elemAt (elemAt (split "^([^\.]+)" (parseDrvName value.name).name) 1) 0;
     in { inherit value name; };
 in listToAttrs (map importTemplate [
