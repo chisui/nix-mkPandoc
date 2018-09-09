2fa5a03c85dbc9d041a04bf6394547997f31be58
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-09-09 13:59:33 +0200

    support 'include-after-body'

--- mkPandoc.nix
+++ mkPandoc.nix
@@ -16,13 +16,14 @@
 # a list of pandoc filters. Each of these will result in a `--filter` argument
 # to pandoc. The filters themselves have to be deriviations themselves and are
 # added to the buildInputs. The name is either the contents of 
-# `pandocFilterName` on the filter or the derivation name if no explict name was given.
+# `pandocFilterName` on the filter or the derivation name if no explict name was
+# given.
 , filters ? []
 # The template has to point to a latex file and has to list all required
 # texlive packages in an attribute called `texlivePackages`.
 , template ? null
-# Overrides for yaml metadata. Each non null entry in the record will result in a
-# `--metadata=<KEY>:<VALUE>` argument. Boolean entries will result in a
+# Overrides for yaml metadata. Each non null entry in the record will result in
+# a `--metadata=<KEY>:<VALUE>` argument. Boolean entries will result in a
 # `--metadata=<KEY>` flag if the value is `true` otherwise it will be absent
 , metadatas ? {}
 # Template variables. Each entry in the record will result in a
@@ -62,6 +63,7 @@ let
     in mkArgs normalArg (filterAttrs (n: v: elem n [ 
         "from" "to" "bibliography" "csl" "template" "top-level-division"
         "listings" "toc" "number-sections" "verbose" "include-before-body"
+        "include-after-body"
       ]) args)
       ++ mkArgs (varArg "metadata") metadatas
       ++ mkArgs (varArg "variable") variables
