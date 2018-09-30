a404f7d2a7e862161cef838aad25846c4e9a4269
Philipp Dargel <chisui@users.noreply.github.com>
2018-09-30 20:48:59 +0200

    Added documentation to nix expresion

--- templates/default.nix
+++ templates/default.nix
@@ -4,6 +4,7 @@ let
   importTemplate = t: 
     let
       value = import t { inherit pkgs; };
+      # extract `name` from `{name}.latex.nix`
       name = elemAt (elemAt (split "^([^\.]+)" (parseDrvName value.name).name) 1) 0;
     in { inherit value name; };
 in listToAttrs (map importTemplate [
