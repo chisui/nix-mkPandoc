f7cf6f1d4ad182f1869b6a146038a310089e7d02
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-10-02 22:52:04 +0200

    use correct phase in build

--- examples/default.nix
+++ examples/default.nix
@@ -2,19 +2,19 @@
 , mkDerivation ? pkgs.stdenv.mkDerivation
 }:
 mkDerivation {
-  name       = "mkPandoc-examples";
-  version    = "0.1.0";
-  phases     = ["buildPhase"];
-  buildPhase = ''
+  name         = "mkPandoc-examples";
+  version      = "0.1.0";
+  phases       = ["installPhase"];
+  installPhase = ''
     mkdir $out
-    ln -s ${import ./simple.nix {}            } $out/simple.pdf
-    ln -s ${import ./toHtml.nix {}            } $out/toHtml.html
-    ln -s ${import ./withBibliography.nix {}  } $out/withBibliography.pdf
-    ln -s ${import ./withCodeBlocks.nix {}    } $out/withCodeBlocks.pdf
-    ln -s ${import ./withCsl.nix {}           } $out/withCsl.pdf
-    ln -s ${import ./withImage.nix {}         } $out/withImage.pdf
+    ln -s ${import ./simple.nix            {} } $out/simple.pdf
+    ln -s ${import ./toHtml.nix            {} } $out/toHtml.html
+    ln -s ${import ./withBibliography.nix  {} } $out/withBibliography.pdf
+    ln -s ${import ./withCodeBlocks.nix    {} } $out/withCodeBlocks.pdf
+    ln -s ${import ./withCsl.nix           {} } $out/withCsl.pdf
+    ln -s ${import ./withImage.nix         {} } $out/withImage.pdf
     ln -s ${import ./withLatexTemplate.nix {} } $out/withLatexTemplate.pdf
-    ln -s ${import ./withVariables.nix {}     } $out/withVariables.pdf
+    ln -s ${import ./withVariables.nix     {} } $out/withVariables.pdf
   '';
 }
 
