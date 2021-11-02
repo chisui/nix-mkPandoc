ce6798a994dea5e4186e33e6f467bfca3867b715
Philipp Dargel <chisui@users.noreply.github.com>
2021-11-02 16:50:49 +0100

    Update withAdditionalPackges.nix

--- examples/withAdditionalPackges.nix
+++ examples/withAdditionalPackges.nix
@@ -6,6 +6,10 @@ mkPandoc {
   name    = "withAdditionalPackages.pdf";
   version = "0.1.0";
   src     = ./withAdditionalPackges.md;
-  texlivePackages = { inherit (texlive) booktabs; };
+  texlivePackages = {
+    inherit (texlive)
+      xcolor
+      booktabs;
+  };
 }
 
