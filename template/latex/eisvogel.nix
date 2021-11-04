9f462382efdca2eeb28735176311dbf84a6ffa34
Philipp Dargel <chisui.pd@gmail.com>
2021-11-04 18:06:34 +0100

    remove redundant mkDerivation

--- template/latex/eisvogel.nix
+++ template/latex/eisvogel.nix
@@ -1,22 +1,11 @@
-{ pkgs
-, texlive ? pkgs.texlive
-, mkDerivation ? pkgs.stdenv.mkDerivation
-, fetchurl ? pkgs.fetchurl
-}:
-(mkDerivation {
+{ pkgs }:
+(pkgs.fetchurl {
   name = "eisvogel.latex";
-  version = "0.0.1";
-
-  src = fetchurl {
-    url = "https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/50ad594fc57f07cbf3c6a67befb771b5e12c07e7/eisvogel.tex";
-    sha256 = "1wlcbvhcg19cn88vvvsjiz9s3j4ydsigcr2ymdhpynnvpfmybdzf";
-  };
-
-  phases = [ "installPhase" ];
-  installPhase = "cp $src $out";
+  url = "https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/50ad594fc57f07cbf3c6a67befb771b5e12c07e7/eisvogel.tex";
+  sha256 = "1wlcbvhcg19cn88vvvsjiz9s3j4ydsigcr2ymdhpynnvpfmybdzf";
 }) // {
   texlivePackages = {
-    inherit (texlive)
+    inherit (pkgs.texlive)
       caption
       collection-fontsrecommended
       csquotes
@@ -41,4 +30,3 @@
       zref;
   };
 }
-
